472c6eabb90ee89dc020a8f76532288daee2835c
AliceSweeney <alice@sweeneyclan.org>
2025-09-17 13:36:03 -0700

    Feat: add star ratings and make them work on Alice's pages

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-	
+	$(".star-rating span").click(function(){
+  	$(this).css("color", "yellow");
+    $(this).prevAll().css("color", "yellow");
+    $(this).nextAll().css("color", "black");
+  });
 });
