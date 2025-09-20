09da2ba0094d221a4cd26f4ea0e2e5773e6fafa1
MelanieCampuzano <Undepth71@gmail.com>
2025-09-19 18:09:02 -0700

    Feat: add checklist decor

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
 	$(".star-rating span").click(function(){
-  	$(this).css("color", "yellow");
-    $(this).prevAll().css("color", "yellow");
-    $(this).nextAll().css("color", "black");
+  		$(this).css("color", "yellow");
+    	$(this).prevAll().css("color", "yellow");
+    	$(this).nextAll().css("color", "black");
   });
+    $(".checklist").click(function() {
+		$(this).closest(".checklist-row").toggleClass("checkbox", this.checked);
+	});
 });
